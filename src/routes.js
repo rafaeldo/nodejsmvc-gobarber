1f3135f5cc9dc53be1aa8ef1fb7bd33d17afa996
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-02-23 18:27:06 -0300

    custom middlewares > auth and guest created.

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const routes = express.Router()
 
+// Controllers
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+
+// Custom Middlewares
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
 
 // Files Upload
 const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 
-routes.get('/', SessionController.create)
+// ROUTES
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
-routes.get('/signup', UserController.create)
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+// Auth Middleware Guard
+routes.use('/app', authMiddleware)
 
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
