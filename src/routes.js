c1973bb7676351235c4db94d221012922fb4968f
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-02-23 17:02:09 -0300

    Routes > new routes: Sigin and a temporarily route for the app Dashboard

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,19 @@ const express = require('express')
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
 // Files Upload
 const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 
-routes.get('/', (req, res) => {
-  res.render('auth/signup')
-})
-
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.store)
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.get('/app/dashboard', (req, res) => {
+  return res.render('dashboard.njk')
+})
 
 module.exports = routes
